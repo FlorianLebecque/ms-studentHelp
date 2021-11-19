197200c52f2d9cfb176dc3c3c96aa2d3224ac270
Robin Lamotte <17051@ecam.be>
2021-11-19 10:37:32 +0100

    demo test

---
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/HelloController.java
@@ -0,0 +1,21 @@
+package be.ecam.ms_studenthelp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/helloworld")
+	public String index_bis() {
+		return "HELLO SPRINGBOOTS!!!";
+	}
+
+
+
+}
