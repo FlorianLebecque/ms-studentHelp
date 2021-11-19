b80972bdb259b0d772475e8128da3e63196c58af
Lebecque Florian <Florianlebecque@hotmail.com>
2021-11-19 10:20:07 +0100

    Init java project

---
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/MsStudenthelpApplication.java
@@ -0,0 +1,13 @@
+package be.ecam.ms_studenthelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStudenthelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStudenthelpApplication.class, args);
+	}
+
+}
