da3d4c05e81b7c43ef44bf1b2ba3d3b39761da55
Louis-Antoine DEVOS <18168@ecam.be>
2021-11-26 12:06:05 +0100

    import @requestParam

--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/HelloController.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/HelloController.java
@@ -1,6 +1,7 @@
 package be.ecam.ms_studenthelp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
