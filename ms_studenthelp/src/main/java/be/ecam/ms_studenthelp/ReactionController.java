326bc730da49ea75ca3d17580c3768d16395b84f
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2021-11-26 11:14:26 +0100

    Add GET handler for reactions

---
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/ReactionController.java
@@ -0,0 +1,33 @@
+package be.ecam.ms_studenthelp;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.ecam.ms_studenthelp.Object.Post;
+import be.ecam.ms_studenthelp.Object.Reaction;
+
+@RestController
+public class ReactionController {
+
+	@GetMapping("/posts/{postId}/reactions")
+	public GetPostsPostIdReactionsResult getPostsPostIdReactions(@PathVariable("postId") String postId) {
+		Reaction r1 = new Reaction(postId, "efgh", 1);
+		Reaction r2 = new Reaction(postId, "ijkl", -1);
+		ArrayList r = new ArrayList<Reaction>();
+		r.add(r1);
+		r.add(r2);
+		return new GetPostsPostIdReactionsResult(r);
+	}
+
+	class GetPostsPostIdReactionsResult {
+		public ArrayList<Reaction> data;
+
+		public GetPostsPostIdReactionsResult(ArrayList<Reaction> data) {
+			this.data = data;
+		}
+	}
+
+}
