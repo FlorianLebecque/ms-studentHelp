1b91fcedb6c08151ca7a9411b2da760eacab1dab
florian lebecque <florianlebecque@hotmail.com>
2021-11-29 09:13:52 +0100

    Now use interface

--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/PostController.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/PostController.java
@@ -3,26 +3,27 @@ package be.ecam.ms_studenthelp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import be.ecam.ms_studenthelp.Interfaces.IPost;
 import be.ecam.ms_studenthelp.Object.Post;
+
 
 @RestController
 public class PostController {
 
     @GetMapping("/getPost")
-    public Post GetPost(){
-        Post post = new Post();
+    public IPost GetPost(){
+        IPost post = new Post();
         post.setAuthorId("L-A");
         post.setContent("Coucou");
         return post;
     }
 
     @GetMapping("/helloMapping")
-	public Post index_ter() {
+	public IPost index_ter() {
 
-        MsStudenthelpApplication.DatabaseManager.CreateForumThread(ft);
+        //MsStudenthelpApplication.DatabaseManager.CreateForumThread(ft);
 
-        Post test = new Post();
+        IPost test = new Post();
         test.setAuthorId("kamoulox");
         test.setContent("C'est la vie de chateau les blEUs?!!");
         //const myJSON = JSON.stringify(obj)
