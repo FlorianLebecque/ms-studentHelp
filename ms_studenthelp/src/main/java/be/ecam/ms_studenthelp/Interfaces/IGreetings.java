603883257d7d899008bde5316ed292d27611a2db
florian lebecque <florianlebecque@hotmail.com>
2021-12-03 08:52:56 +0100

    Converted greetings to interface

---
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/Interfaces/IGreetings.java
@@ -0,0 +1,8 @@
+package be.ecam.ms_studenthelp.Interfaces;
+
+public interface IGreetings {
+    
+    public long getId();
+
+    public String getContent();
+}
--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/Object/Greeting.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/Object/Greeting.java
@@ -1,9 +1,11 @@
 package be.ecam.ms_studenthelp.Object;
 //package be.ecam.ms_studenthelp.restservice;
 
+import be.ecam.ms_studenthelp.Interfaces.IGreetings;
+
 //Example with param
 
-public class Greeting {
+public class Greeting implements IGreetings{
 
     private final long id;
     private final String content;
