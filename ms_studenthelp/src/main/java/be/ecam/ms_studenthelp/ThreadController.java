161e2fe79dd43c0c3433c8a7d87c105d4a3a9745
SarahLiettefti <18253@ecam.be>
2021-12-10 10:25:33 +0100

    Creat a Thread usig POST and a body

--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/ThreadController.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/ThreadController.java
@@ -4,10 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import be.ecam.ms_studenthelp.Interfaces.IForumThread;
 import be.ecam.ms_studenthelp.Object.ForumThread;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -30,6 +43,19 @@ public class ThreadController {
         return thread;
     }
 
+    @PostMapping("/createThreadPOST") //Creat a Thread usig POST and a body
+    public IForumThread CreateThreadPOST(@RequestBody String body){
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String,Object> body_data = springParser.parseMap(body);
+        String threadTitre = (String) body_data.get("title");
+        //List<String> tags = (List<String>) body_data.get("tags");
+        String NomDeCatégorie = (String) body_data.get("category");
+        String AutheurID = "Someone"; //To be set : the author is the authenticated user
+        IForumThread thread = new ForumThread(threadTitre, AutheurID,NomDeCatégorie);
+        MsStudenthelpApplication.DatabaseManager.CreateForumThread(thread);
+        return thread;
+    }
+
     @GetMapping("/thread/{threadId}")
     //test id : 11815501-d8d1-4c05-a0a6-59c699064772
     public IForumThread GetThreadByThreadId(@PathVariable("threadId") String threadId) {
@@ -37,6 +63,7 @@ public class ThreadController {
         System.out.println(thread);
         return thread;
     }
+
     @GetMapping("/thread/UpdateTitle/{threadId}/{newTitle}")
     public IForumThread UpdateForumThreadTitle(@PathVariable("threadId") String threadId, @PathVariable("newTitle") String newTitle) {
         IForumThread thread = MsStudenthelpApplication.DatabaseManager.GetForumThread(threadId);
