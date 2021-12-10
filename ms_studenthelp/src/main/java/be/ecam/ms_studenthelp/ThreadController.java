657bc6173ee2c7f44db9fb9ad2a52555fef88a7e
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2021-12-10 12:07:31 +0100

    WIP: goto eat;

--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/ThreadController.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/ThreadController.java
@@ -8,7 +8,9 @@ import java.util.Map;
 import java.util.UUID;
 
 import be.ecam.ms_studenthelp.Interfaces.IForumThread;
+import be.ecam.ms_studenthelp.Interfaces.IPost;
 import be.ecam.ms_studenthelp.Object.ForumThread;
+import be.ecam.ms_studenthelp.Object.Post;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
@@ -16,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,73 +29,128 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class ThreadController {
 
-    @GetMapping("/CreateThread")
-    public IForumThread CreateThreadByDefault(){
-        IForumThread thread = new ForumThread("Un tres bon titre","Quelqu1","Une catégorie banale");
-        MsStudenthelpApplication.DatabaseManager.CreateForumThread(thread);
-        return thread;
-    }
-
-    @GetMapping("/createThread/{threadTitre}/{AutheurID}/{NomDeCatégorie}")
-    //createThread/BeauTitre/Sarah/cathegory1
-    public IForumThread CreateThread(@PathVariable("threadTitre") String threadTitre,
-                                            @PathVariable("AutheurID") String AutheurID,
-                                            @PathVariable("NomDeCatégorie") String NomDeCatégorie){
-        IForumThread thread = new ForumThread(threadTitre, AutheurID,NomDeCatégorie);
-        MsStudenthelpApplication.DatabaseManager.CreateForumThread(thread);
-        return thread;
-    }
-
-    @PostMapping("/createThreadPOST") //Creat a Thread usig POST and a body
-    public IForumThread CreateThreadPOST(@RequestBody String body){
+    /**
+     * POST /threads
+     * https://beta.bachelay.eu/ms-studentHelp/#/operations/post-threads
+     */
+    @PostMapping("/threads")
+    public IForumThread PostThreads(@RequestBody String body){
         JsonParser springParser = JsonParserFactory.getJsonParser();
         Map<String,Object> body_data = springParser.parseMap(body);
-        String threadTitre = (String) body_data.get("title");
-        ArrayList<String> tags = (ArrayList<String>) body_data.get("tags");
-        String NomDeCatégorie = (String) body_data.get("category");
-        String AutheurID = "Someone"; //To be set : the author is the authenticated user
-        IForumThread thread = new ForumThread(threadTitre, AutheurID,NomDeCatégorie);
+
+        // ForumThread info
+        String title = (String) body_data.get("title");
+        List<String> tags = (List<String>) body_data.get("tags");
+        String category = (String) body_data.get("category");
+        Boolean answered = (Boolean) body_data.get("answered");
+
+        if (title == null || tags == null || category == null) {
+            return null; // TODO: proper error handling, Bad Request
+        }
+        if (answered == null) {
+            answered = false;
+        }
+
+        // child Post info
+        Map<String,String> first_post = (Map<String,String>) body_data.get("first_post");
+        if (first_post == null) {
+            return null; // TODO: proper error handling, Bad Request
+        }
+        String content = first_post.get("content");
+
+        if (content == null) {
+            return null; // TODO: proper error handling, Bad Request
+        }
+
+        String authorID = "Someone"; // TODO : the author is the authenticated user
+
+
+        IPost post = new Post(authorID, content);
+        IForumThread thread = new ForumThread(title, authorID, category, post, answered);
         thread.AddTags(tags);
+
+        MsStudenthelpApplication.DatabaseManager.CreatePost(post);
         MsStudenthelpApplication.DatabaseManager.CreateForumThread(thread);
+
         return thread;
     }
 
-    @GetMapping("/thread/{threadId}")
-    //test id : 11815501-d8d1-4c05-a0a6-59c699064772
-    public IForumThread GetThreadByThreadId(@PathVariable("threadId") String threadId) {
+    /**
+     * GET /threads/{threadId}
+     * https://beta.bachelay.eu/ms-studentHelp/#/operations/get-threads-threadId
+     */
+    @GetMapping("/threads/{threadId}")
+    public IForumThread getThreadsThreadId(@PathVariable("threadId") String threadId) {
         IForumThread thread = MsStudenthelpApplication.DatabaseManager.GetForumThread(threadId);
-        System.out.println(thread);
         return thread;
     }
 
-    @GetMapping("/thread/UpdateTitle/{threadId}/{newTitle}")
-    public IForumThread UpdateForumThreadTitle(@PathVariable("threadId") String threadId, @PathVariable("newTitle") String newTitle) {
+    /**
+     * PATCH /threads/{threadId}
+     * https://beta.bachelay.eu/ms-studentHelp/#/operations/patch-threads-threadId
+     */
+    @PatchMapping("/threads/{threadId}")
+    public IForumThread patchThreadsThreadId(@PathVariable("threadId") String threadId, @RequestBody String body) {
         IForumThread thread = MsStudenthelpApplication.DatabaseManager.GetForumThread(threadId);
-        thread.UpdateTitle(newTitle);
+
+        if (thread == null) {
+            return null; // TODO: proper error handling, Not Found
+        }
+
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String,Object> body_data = springParser.parseMap(body);
+
+        // ForumThread info
+        String title = (String) body_data.get("title");
+        List<String> tags = (List<String>) body_data.get("tags");
+        String category = (String) body_data.get("category");
+        Boolean answered = (Boolean) body_data.get("answered");
+
+        if (title != null) {
+            thread.UpdateTitle(title);
+        }
+        if (tags != null) {
+            thread.AddTags(tags);
+        }
+        if (category != null) {
+            thread.UpdateCategory(category);
+        }
+        if (answered != null) {
+            thread.UpdateAnswered(answered);
+        }
+
         MsStudenthelpApplication.DatabaseManager.UpdateForumThread(thread);
         return thread;
     }
 
-    @GetMapping("/thread/UpdateCategory/{threadId}/{newCategory}")
-    public IForumThread UpdateForumThreadCategory(@PathVariable("threadId") String threadId, @PathVariable("newCategory") String newCategory) {
-        IForumThread thread = MsStudenthelpApplication.DatabaseManager.GetForumThread(threadId);
-        thread.UpdateCategory(newCategory);
-        MsStudenthelpApplication.DatabaseManager.UpdateForumThread(thread);
-        return thread;
-    }
-
-    @GetMapping("/thread/Delete/{threadId}")
+    /**
+     * DELETE /threads/{threadId}
+     * https://beta.bachelay.eu/ms-studentHelp/#/operations/delete-threads
+     */
+    @DeleteMapping("/threads/{threadId}")
     public IForumThread DeleteForumThreadTitle(@PathVariable("threadId") String threadId) {
         IForumThread thread = MsStudenthelpApplication.DatabaseManager.GetForumThread(threadId);
+        if (thread == null) {
+            return null; // Idempotency, if it doesn't exist, it is deleted
+        }
         thread.UpdateTitle("Deleted");
         MsStudenthelpApplication.DatabaseManager.UpdateForumThread(thread);
-        return thread;
+
+        IPost post = thread.getChild();
+        if (post == null) {
+            return null; // Idempotency, if it doesn't exist, it is deleted
+        }
+        MsStudenthelpApplication.DatabaseManager.UpdatePost(post);
+
+        return null;
     }
 
+    /*
     @GetMapping("/thread/GetForumPages/{nbr_per_page}/{page_index}")
     public List<IForumThread> GetForumThreadPages(@PathVariable("nbr_per_page") int nbr_per_page, @PathVariable("page_index") int page_index) {
         List<IForumThread> ft_list = MsStudenthelpApplication.DatabaseManager.GetForumThreads(nbr_per_page,page_index);
         return ft_list;
     }
+    */
 
 }
