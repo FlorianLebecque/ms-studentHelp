b765e4e30fb19297e48df672ef5837eb4939cbfb
Robin Lamotte <17051@ecam.be>
2021-12-17 09:10:26 +0100

    mise en forme commentaire Post

--- ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/Object/Post.java
+++ ms_studenthelp/src/main/java/be/ecam/ms_studenthelp/Object/Post.java
@@ -11,8 +11,22 @@ import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Post implements IPost{
-    //A Post is created by defining authorId and content before defining the rest via the setters
-    //To create a reply: create a new Post before using function Reply(IPost reply) which defines the new Post as beeing the reply
+    /**
+     * The Post object represents a post inside a parent thread. 
+     * He can have answers that will be referenced in his 'children' list.
+     * 2 ways to create a Post object:
+     *  CREATE: when you create a new Post
+     *      @param {String} _authorId
+     *      @param {String} _content
+     *      Other @parameters are defined by setters
+     *  LOAD: when you load an existing Post from the database
+     *      @param {String} id_
+     *      @param {String} authorId_
+     *      @param {Stirg} content_
+     *      @param {LocalDateTime} date_
+     *      @param {LocalDateTime} LastModif_
+     *      @param {IPost} parent_
+     */
 
     private final String id;
     private final String authorId;
@@ -29,13 +43,13 @@ public class Post implements IPost{
     public List<IPost> children = new ArrayList<IPost>();
 
     //create
-    public Post(String _authorId,String content_){
+    public Post(String _authorId,String _content){
         id    = GuidGenerator.GetNewUUIDString();
         authorId = _authorId;
 
         date  = LocalDateTime.now();
 
-        content = content_;
+        content = _content;
 
     }
 
@@ -113,6 +127,7 @@ public class Post implements IPost{
         UpdateDate();
     }
 
+    //To create a reply: create a new Post before using function Reply(IPost reply) which defines the new Post as beeing the reply
     public void Reply(IPost reply){
         reply.setParent(this);
         children.add(reply);
